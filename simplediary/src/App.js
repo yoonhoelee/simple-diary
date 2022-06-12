4de123bc585b852bfe65477b6369ed07be95b40b
yoonhoelee <ubrain0624@gmail.com>
2022-06-12 09:00:45 +0900

    added diaryList

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
-
+import DiaryList from "./DiaryList";
+const dummyList=[
+    {
+        id:1,
+        author:"이윤회",
+        content: "yeah",
+        emotion: 5,
+        created_date: new Date().getTime()
+    },
+    {
+        id:2,
+        author:"Adam",
+        content: "yeah 1",
+        emotion: 4,
+        created_date: new Date().getTime()
+    }
+]
 function App() {
   return (
     <div className="App">
         <DiaryEditor/>
+        <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
